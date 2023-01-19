a79bdb27ef5dc763c53c103ea38108a03ef4cedd
Michael Elias <melias@nordicnaturals.com>
2023-01-18 17:32:31 -0800

    Some clean up and more style updates

--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -1,29 +1,43 @@
 import { Pokemon } from "../types/pokemon";
 
 const PokeCard = ({ pokemon }: { pokemon: Pokemon }) => {
+  // handle card flip
+  const handleCardFlip = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const card = e.currentTarget;
+    card.classList.toggle("active");
+  };
+
   return (
-    <div className="Card">
-      <div className="Card__img">
-        <img src={pokemon.sprites.front_default} alt="" />
-      </div>
-      <div className="Card__name">{pokemon.name}</div>
-      <div className="Card__types">
-        {pokemon.types.map((type) => {
-          return <div className="Card__type">{type.type.name}</div>;
-        })}
-      </div>
-      <div className="Card__info">
-        <div className="Card__data Card__data--weight">
-          <p className="title">Weight</p>
-          <p>{pokemon.weight}</p>
+    <div className="flip" onClick={handleCardFlip}>
+      <div className="front">
+        <div className={`bg-white shadow-md text-secondary bg-opacity-60 px-2 py-2 rounded-xl transition-all hover:rotate-3 hover:-translate-y-1`}>
+          <div className="h-[330px] bg-blue-200 p-3 relative rounded bg-opacity-70">
+            <small className="poke-id">#{pokemon.id}</small>
+            <h3 className="poke-name capitalize font-bold">{pokemon.name} </h3>
+            <img loading="eager" className="ml-auto mr-auto mb-3" width="175" height="175" src={pokemon.sprites.other?.["official-artwork"].front_default} alt={pokemon.name} />
+            {pokemon.types.map((type) => (
+              <span key={type.slot} className={`pokemon-type ${type.type.name}`}>{type.type.name}</span>
+            ))}
+            <p className="text-sm leading-5 mt-3 mb-2">
+              <b>Abilities: </b>
+              {pokemon.abilities.map((ability) => ability.ability.name).join(", ")}
+            </p>
+            {/* <p className="text-sm font-medium mt-2"> Base exp: {pokemon.base_experience} </p> */}
+          </div>
         </div>
-        <div className="Card__data Card__data--weight">
-          <p className="title">Height</p>
-          <p>{pokemon.height}</p>
-        </div>
-        <div className="Card__data Card__data--ability">
-          <p className="title">Ability</p>
-          <p>{pokemon.abilities[0].ability.name}</p>
+      </div>
+      <div className="back">
+        <div key={pokemon.id} className={`bg-white shadow-md text-secondary bg-opacity-60 px-2 py-2 rounded-xl transition-all`}>
+          <div className="h-[330px] bg-blue-200 p-3 rounded">
+          <h3 className="capitalize mb-2 font-bold">{pokemon.name} <small className="text-xs">#{pokemon.id}</small></h3>
+          <img loading="lazy" className="ml-auto mr-auto" width="96" height="96" src={pokemon.sprites.back_default} alt={pokemon.name} />
+            <h4 className="font-bold text-left border-b-2 border-blue-300 -mt-2">Stats</h4>
+            <ul className="p-0">
+            {pokemon.stats.map((stat) => (
+              <li key={stat.stat.name} className="text-secondary m-1 text-left leading-4 font-semibold text-sm">{stat.stat.name}: {stat.base_stat}</li>
+            ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { Pokemon, Result } from "../types/pokemon"
+import PokeCard from "./PokeCard";
+import Loading from "./Loading";
 
 interface PokemonResults {
   pokemonList: Pokemon[];
@@ -55,6 +57,7 @@ const PokemonList: React.FC = () => {
     queryKey: ["pokemonList", page],
     queryFn: getPokemonList,
     keepPreviousData: true,
+    staleTime: 1000 * 60 * 60 * 24,
   }
   );
 
@@ -71,60 +74,25 @@ const PokemonList: React.FC = () => {
   return (
     <div>
       {isInitialLoading ? (
-        <div role="status">
-          <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-400 fill-yellow-200" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-          </svg>
-            <span className="sr-only">Loading...</span>
-        </div>
+        <Loading />
       ) : (
           <>
             <h1 className="poke-title mb-8 mt-2">Pokedex</h1>
-            <div className="grid justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 flex-wrap">
+            <div className="grid max-w-[1100px] justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 flex-wrap">
             {(data as Pokemon[]).map((pokemon) => (
-              <div className="flip">
-                <div className="front">
-                  <div key={pokemon.id} className={`bg-white shadow-md text-secondary bg-opacity-60 px-2 py-2 rounded-xl transition-all hover:rotate-3 hover:-translate-y-1`}>
-                    <div className="h-[330px] bg-blue-200 p-3 relative rounded bg-opacity-70">
-                      <small className="poke-id">#{pokemon.id}</small>
-                      <h3 className="poke-name capitalize font-bold">{pokemon.name} </h3>
-                      <img loading="eager" className="ml-auto mr-auto mb-3" width="175" height="175" src={pokemon.sprites.other?.["official-artwork"].front_default} alt={pokemon.name} />
-                      {pokemon.types.map((type) => (
-                        <span key={type.slot} className={`pokemon-type ${type.type.name}`}>{type.type.name}</span>
-                      ))}
-                      <p className="text-sm leading-5 mt-3 mb-2">
-                        <b>Abilities: </b>
-                        {pokemon.abilities.map((ability) => ability.ability.name).join(", ")}
-                      </p>
-                      {/* <p className="text-sm font-medium mt-2"> Base exp: {pokemon.base_experience} </p> */}
-                    </div>
-                  </div>
-                </div>
-                <div className="back">
-                  <div key={pokemon.id} className={`bg-white shadow-md text-secondary bg-opacity-60 px-2 py-2 rounded-xl transition-all`}>
-                    <div className="h-[330px] bg-blue-200 p-3 rounded">
-                    <h3 className="capitalize mb-2 font-bold">{pokemon.name} <small className="text-xs">#{pokemon.id}</small></h3>
-                    <img loading="lazy" className="ml-auto mr-auto" width="96" height="96" src={pokemon.sprites.back_default} alt={pokemon.name} />
-                      <h4 className="font-bold text-left border-b-2 border-blue-300 -mt-2">Stats</h4>
-                      <ul className="p-0">
-                      {pokemon.stats.map((stat) => (
-                        <li key={stat.stat.name} className="text-secondary m-1 text-left leading-4 font-semibold text-sm">{stat.stat.name}: {stat.base_stat}</li>
-                      ))}
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <PokeCard key={pokemon.id} pokemon={pokemon} />
             ))}
             </div>
-            <div className="pagination flex justify-between mb-4 mt-4">
+            <div className="pagination flex justify-between mb-4 mt-10">
               <button className="btn btn-secondary" onClick={() => setPage((old) => Math.max(old - 1, 0))} disabled={page === 0}>
                 Previous
               </button>{' '}
-                <span className={`flex items-center justify-center bg-[#f24646] w-12 h-12 border-white -translate-y-1 border-solid border-2 rounded-full font-bold py-[10px] px-[17px] shadow-sm text-white inline-block`}>
+              <div className="flex items-center flex-col -translate-y-4 ">
+                <small className="text-green-900 uppercase font-semibold">Page</small>
+                <span className={`flex items-center justify-center bg-[#f24646] w-12 h-12 border-white border-solid border-2 rounded-full font-bold py-[10px] px-[17px] shadow-sm text-white inline-block`}>
                   {isPreviousData ? <Spinner /> : page + 1 }
                 </span>
+              </div>
               <button
                 className="btn btn-secondary relative"
                 onClick={() => {
