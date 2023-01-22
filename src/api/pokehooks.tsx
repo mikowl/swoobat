fa30f087259b611eaaf87de913fe5f69e5fc19dc
mikowl <michael@mikowl.com>
2023-01-22 09:14:27 -0800

    Move color card function to hooks file even though its not really a hook whatever

--- src/api/pokehooks.tsx
+++ src/api/pokehooks.tsx
@@ -39,4 +39,47 @@ const usePokemon = (page: number): UseQueryResult<Pokemon[], Error> => {
 	});
 };
 
-export { getPokemonList, usePokemon };
+const getCardColor = (type: string) => {
+	switch (type) {
+		case "grass":
+			return "bg-green-200";
+		case "fire":
+			return "bg-orange-200";
+		case "water":
+			return "bg-blue-200";
+		case "bug":
+			return "bg-lime-200";
+		case "normal":
+			return "bg-gray-200";
+		case "poison":
+			return "bg-purple-200";
+		case "electric":
+			return "bg-yellow-200";
+		case "ground":
+			return "bg-[#cebb8db3]";
+		case "fairy":
+			return "bg-pink-200";
+		case "fighting":
+			return "bg-red-200";
+		case "psychic":
+			return "bg-pink-200";
+		case "rock":
+			return "bg-yellow-200";
+		case "ghost":
+			return "bg-gray-200";
+		case "ice":
+			return "bg-blue-200";
+		case "dragon":
+			return "bg-emerald-200";
+		case "flying":
+			return "bg-sky-200";
+		case "dark":
+			return "bg-gray-200";
+		case "steel":
+			return "bg-gray-200";
+		default:
+			return "bg-gray-200";
+	}
+};
+
+export { getPokemonList, usePokemon, getCardColor };
